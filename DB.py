5f4adb895340e83f8944ca689229339de77d5f30
Haidara <haidaraibrahim963938@gmail.com>
2024-05-02 22:45:58 +0300

    Initial commit

---
+++ DB.py
@@ -0,0 +1,121 @@
+import sqlite3
+import os
+import re
+from asyncio import Lock
+
+lock = Lock()
+
+
+def lock_and_release(func):
+    async def wrapper(*args, **kwargs):
+        db = None
+        cr = None
+        try:
+            await lock.acquire()
+            db = sqlite3.connect(os.getenv("DB_PATH"))
+            db.row_factory = sqlite3.Row
+            cr = db.cursor()
+            result = await func(*args, **kwargs, cr=cr)
+            db.commit()
+            if result:
+                return result
+        except sqlite3.Error as e:
+            print(e)
+        finally:
+            cr.close()
+            db.close()
+            lock.release()
+
+    return wrapper
+
+
+def connect_and_close(func):
+    def wrapper(*args, **kwargs):
+        db = sqlite3.connect(os.getenv("DB_PATH"))
+        db.row_factory = sqlite3.Row
+        db.create_function("REGEXP", 2, regexp)
+        cr = db.cursor()
+        result = func(*args, **kwargs, cr=cr)
+        cr.close()
+        db.close()
+        return result
+
+    return wrapper
+
+
+def regexp(expr, item):
+    reg = re.compile(expr)
+    return reg.search(item) is not None
+
+
+class DB:
+
+    @staticmethod
+    def creat_tables():
+        db = sqlite3.connect(os.getenv("DB_PATH"))
+        cr = db.cursor()
+        script = f"""
+
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            username TEXT UNIQUE,
+            name TEXT
+        );
+
+        CREATE TABLE IF NOT EXISTS admins (
+            id INTEGER PRIMARY KEY
+        );
+
+        INSERT OR IGNORE INTO admins(id) VALUES({int(os.getenv('OWNER_ID'))});
+        """
+        cr.executescript(script)
+
+        db.commit()
+        cr.close()
+        db.close()
+
+    @staticmethod
+    @connect_and_close
+    def check_admin(user_id: int, cr: sqlite3.Cursor = None):
+        cr.execute("SELECT * FROM admins WHERE id=?", (user_id,))
+        return cr.fetchone()
+
+    @staticmethod
+    @connect_and_close
+    def get_admin_ids(cr: sqlite3.Cursor = None):
+        cr.execute("SELECT * FROM admins")
+        return cr.fetchall()
+
+    @staticmethod
+    @lock_and_release
+    async def add_new_user(
+        user_id: int, username: str, name: str, cr: sqlite3.Cursor = None
+    ):
+        username = username if username else " "
+        name = name if name else " "
+        cr.execute(
+            "INSERT OR IGNORE INTO users(id, username, name) VALUES(?, ?, ?)",
+            (user_id, username, name),
+        )
+
+    @staticmethod
+    @lock_and_release
+    async def add_new_admin(user_id: int, cr: sqlite3.Cursor = None):
+        cr.execute("INSERT OR IGNORE INTO admins(id) VALUES(?)", (user_id,))
+
+    @staticmethod
+    @lock_and_release
+    async def remove_admin(user_id: int, cr: sqlite3.Cursor = None):
+        cr.execute("DELETE FROM admins WHERE id = ?", (user_id,))
+
+    @staticmethod
+    @connect_and_close
+    def get_user(user_id: int, cr: sqlite3.Cursor = None):
+        cr.execute("SELECT * FROM users WHERE id = ?", (user_id,))
+        return cr.fetchone()
+
+    @staticmethod
+    @connect_and_close
+    def get_all_users(cr: sqlite3.Cursor = None):
+        cr.execute("SELECT * FROM users")
+        return cr.fetchall()
---
+++ handlers.py
@@ -0,0 +1,78 @@
+from telegram import (
+    Update,
+)
+
+from telegram.ext import (
+    CallbackQueryHandler,
+    Application,
+    PicklePersistence,
+    InvalidCallbackData,
+    Defaults,
+)
+
+from telegram.constants import (
+    ParseMode,
+)
+
+from start import (
+    start_command,
+    check_joined_handler,
+    inits,
+)
+
+from common import (
+    back_to_user_home_page_handler,
+    back_to_admin_home_page_handler,
+    invalid_callback_data,
+    error_handler,
+)
+
+from user.user_calls import *
+
+from admin.admin_calls import *
+from admin.admin_settings import *
+from admin.broadcast import *
+
+import os
+from DB import DB
+
+
+def main():
+    DB.creat_tables()
+    defaults = Defaults(parse_mode=ParseMode.HTML)
+    my_persistence = PicklePersistence(filepath="data/persistence", single_file=False)
+    app = (
+        Application.builder()
+        .token(os.getenv("BOT_TOKEN"))
+        .post_init(inits)
+        .arbitrary_callback_data(True)
+        .persistence(persistence=my_persistence)
+        .defaults(defaults)
+        .concurrent_updates(True)
+        .build()
+    )
+
+    app.add_handler(
+        CallbackQueryHandler(
+            callback=invalid_callback_data, pattern=InvalidCallbackData
+        )
+    )
+    # ADMIN SETTINGS
+    app.add_handler(admin_settings_handler)
+    app.add_handler(show_admins_handler)
+    app.add_handler(add_admin_handler)
+    app.add_handler(remove_admin_handler)
+
+    app.add_handler(broadcast_message_handler)
+
+    app.add_handler(check_joined_handler)
+
+    app.add_handler(start_command)
+    app.add_handler(find_id_handler)
+    app.add_handler(hide_ids_keyboard_handler)
+    app.add_handler(back_to_user_home_page_handler)
+    app.add_handler(back_to_admin_home_page_handler)
+
+    app.add_error_handler(error_handler)
+
+    app.run_polling(allowed_updates=Update.ALL_TYPES)
